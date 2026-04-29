40adc0a28b44ca481b9678ade593999ca32d974d
ANN <gpreetham@MacBook-Air-174.local>
2026-04-29 18:31:14 -0400

    Fix: remove vulnerable demo SQL code (use parameterized queries)

--- vulnerable_sql.py
+++
@@ -1,8 +0,0 @@
-# Demo vulnerable SQL code (for Semgrep demo)
-
-def get_user(db, username):
-    # Unsafe: direct string formatting into SQL
-    query = "SELECT * FROM users WHERE username = '%s'" % username
-    print("Executing:", query)
-    return db.execute(query)
-
